2b956e0d4869eae2592e49523569346bb1d36540
Emir Marques <emirdeliz@gmail.com>
2022-08-28 21:53:31 -0300

    Revert "feat: Removed the obsolete folder"
    This reverts commit 36af10ee21782350b3467631a1e08b57dc341643.

---
+++ lib/generated_plugin_registrant.dart
@@ -0,0 +1,17 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore_for_file: directives_ordering
+// ignore_for_file: lines_longer_than_80_chars
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:barcode_reader_webassembly/barcode_reader_webassembly_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(Registrar registrar) {
+  BarcodeReaderWebassemblyWeb.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
