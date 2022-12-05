b3edebc5f778bb32e5146138069221941b5bde89
emirdeliz <emir.marques@condoconta.com>
2022-12-05 16:15:20 -0300

    feat: Added the script to install chrome

---
+++ scripts/run-chrome-install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+apt-get install -y wget &&
+	wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&
+	apt-get install ./google-chrome-stable_current_amd64.deb
