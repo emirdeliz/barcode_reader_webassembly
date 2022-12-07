c2bab2b65cb1d9e280018110536ba489f2883123
emirdeliz <emir.marques@condoconta.com>
2022-12-07 15:25:27 -0300

    feat: Work in progress

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -4,5 +4,5 @@ flutter drive \
 	--driver=driver_test/integration_test.dart \
 	--target=integration_test/barcode_reader_webassembly_test.dart \
 	--dart-define=TEST_ENV=true \
-	-d chrome \
-	--web-port=9191 --browser-name=chrome --headless
+	-d web-server \
+	--release --web-port=9191 --browser-name=chrome --headless
