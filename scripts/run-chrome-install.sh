17cef127b61677e4dbc84aa614838a276799d0d6
emirdeliz <emir.marques@condoconta.com>
2022-12-07 15:15:44 -0300

    feat: Work in progress

--- scripts/run-chrome-install.sh
+++ scripts/run-chrome-install.sh
@@ -5,9 +5,9 @@ apt-get update &&
 	wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - &&
 	echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >>/etc/apt/sources.list.d/google.list &&
 	apt-get update -y &&
-	apt-get install -y google-chrome-stable &&
+	apt-get install -y google-chrome-stable nodejs &&
 	CHROMEVER=$(google-chrome --product-version | grep -o "[^\.]*\.[^\.]*\.[^\.]*") &&
 	DRIVERVER=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROMEVER") &&
-	pwd && mkdir -p driver_test/chromedriver && cd driver_test/chromedriver &&
+	mkdir -p driver_test/chromedriver && cd driver_test/chromedriver &&
 	wget -q --continue "http://chromedriver.storage.googleapis.com/$DRIVERVER/chromedriver_linux64.zip" &&
 	unzip chromedriver*
