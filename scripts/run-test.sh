eb0ea5ad1c055ae335827283986f9d43b8fdfc68
emirdeliz <emir.marques@condoconta.com>
2022-12-07 19:04:01 -0300

    feat: Work in progress

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-mkdir web &&
-	flutter drive \
-		--driver=driver_test/integration_test.dart \
-		--target=integration_test/barcode_reader_webassembly_test.dart \
-		--dart-define=TEST_ENV=true \
-		-d web-server --release
+flutter drive \
+	--driver=driver_test/integration_test.dart \
+	--target=integration_test/barcode_reader_webassembly_test.dart \
+	--dart-define=TEST_ENV=true \
+	-d web-server --release
