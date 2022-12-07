1076b3b58ddc2901e0981dfaacb78303f4c291cf
emirdeliz <emir.marques@condoconta.com>
2022-12-07 11:03:51 -0300

    feat: Work in progress

--- driver_test/run_driver.dart
+++
@@ -1,9 +0,0 @@
-import 'package:web_driver_installer/chrome_driver_installer.dart';
-
-const driverVersion = '108.0.5359.71';
-
-Future<void> main() async {
-  ChromeDriverInstaller command =
-      ChromeDriverInstaller.withVersion(driverVersion);
-  await command.start(alwaysInstall: true);
-}
