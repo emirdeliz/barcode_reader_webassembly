99c39384468174bad50f01ba8909db60f7f75a74
emirdeliz <emir.marques@condoconta.com>
2022-12-08 10:02:33 -0300

    feat: Work in progress

--- scripts/exec-chrome-install.sh
+++ scripts/exec-chrome-install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt-get update &&
+apt-get update &&
 	apt-get install -y gnupg wget curl unzip --no-install-recommends &&
 	wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - &&
 	echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >>/etc/apt/sources.list.d/google.list &&
--- scripts/exec-test.sh
+++ scripts/exec-test.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-flutter drive \
+
+mkdir web && flutter drive \
 	--driver=driver_test/integration_test.dart \
 	--target=integration_test/barcode_reader_webassembly_test.dart \
 	--dart-define=TEST_ENV=true \
